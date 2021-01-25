cd8122a473757aa08abeb1b83a9393f2f53c888f
yanghaonan12 <y13895317762@126.com>
2021-01-25 22:44:04 +0800

    更新1

--- HelloGit/src/main/java/com/yhn/git/HelloGit/HelloGit.java
+++ HelloGit/src/main/java/com/yhn/git/HelloGit/HelloGit.java
@@ -7,5 +7,6 @@ package com.yhn.git.HelloGit;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("更新1");
     }
 }

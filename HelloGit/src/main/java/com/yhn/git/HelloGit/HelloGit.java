0e5d754db55d702e46e08d4cb8c389ddea457e04
yanghaonan12 <y13895317762@126.com>
2021-01-25 22:42:39 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/yhn/git/HelloGit/HelloGit.java
@@ -0,0 +1,11 @@
+package com.yhn.git.HelloGit;
+
+/**
+ * @author YHN
+ * @create 2021-01-25 22:22
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
